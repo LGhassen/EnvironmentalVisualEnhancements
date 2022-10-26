9ba1b1931b0384b0294bbfcdc4f5e27c3f63045b
lghassen <feeldapowa@hotmail.com>
2022-10-26 20:55:13 +0200

    Add event to tell scatterer when to remap clouds

--- Atmosphere/CloudsManager.cs
+++ Atmosphere/CloudsManager.cs
@@ -14,6 +14,13 @@ namespace Atmosphere
         public override ObjectType objectType { get { return ObjectType.BODY | ObjectType.MULTIPLE; } }
         public override String configName { get{return "EVE_CLOUDS";} }
 
+        public static EventVoid onApply;
+
+        public CloudsManager():base()
+        {
+            onApply = new EventVoid("onCloudsApply");
+        }
+
         protected override void ApplyConfigNode(ConfigNode node)
         {
             GameObject go = new GameObject("CloudsManager");
@@ -26,7 +33,7 @@ namespace Atmosphere
 
         protected override void PostApplyConfigNodes()
         {
-
+            onApply.Fire();
         }
 
         protected override void Clean()
