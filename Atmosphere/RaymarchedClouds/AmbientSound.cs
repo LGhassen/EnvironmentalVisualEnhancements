7c40c46fb4f237ae9c0d6ff99337bd0b388c7c12
lghassen <feeldapowa@hotmail.com>
2023-07-02 12:50:25 +0200

    Add separate, optional IVA sounds to ambient sounds

--- Atmosphere/RaymarchedClouds/AmbientSound.cs
+++ Atmosphere/RaymarchedClouds/AmbientSound.cs
@@ -8,19 +8,18 @@ namespace Atmosphere
         [ConfigItem]
         string soundName = "path here";
 
-        //[ConfigItem, Optional]
-        //string ivaSoundName = null;
-
-        public string SoundName { get => soundName; }
-
-        //public string IvaSoundName { get => ivaSoundName; }
+        [ConfigItem, Optional]
+        string ivaSoundName = null;
 
         GameObject ambientSoundGameObject;
-        AudioSource audioSource;
+        AudioSource audioSource = null;
+        AudioSource ivaAudioSource = null;
+
+        bool ivaPlaying = false;
 
         public bool Apply()
         {
-            if (!GameDatabase.Instance.ExistsAudioClip(soundName))// || (IvaSoundName != null && !GameDatabase.Instance.ExistsAudioClip(IvaSoundName)))
+            if (!GameDatabase.Instance.ExistsAudioClip(soundName))
                 return false;
 
             ambientSoundGameObject = new GameObject();
@@ -29,14 +28,24 @@ namespace Atmosphere
                 ambientSoundGameObject.transform.parent = FlightCamera.fetch.transform;
 
             audioSource = ambientSoundGameObject.AddComponent<AudioSource>();
-
             audioSource.clip = GameDatabase.Instance.GetAudioClip(soundName);
             audioSource.loop = true;
 
             audioSource.Play();
             audioSource.Pause();
 
+            if (GameDatabase.Instance.ExistsAudioClip(ivaSoundName))
+            {
+                ivaAudioSource = ambientSoundGameObject.AddComponent<AudioSource>();
+                ivaAudioSource.clip = GameDatabase.Instance.GetAudioClip(ivaSoundName);
+                ivaAudioSource.loop = true;
+
+                ivaAudioSource.Play();
+                ivaAudioSource.Pause();
+            }
+
             GameEvents.onGameSceneLoadRequested.Add(GameSceneLoadStarted);
+            GameEvents.OnCameraChange.Add(CameraChanged);
 
             return true;
         }
@@ -46,20 +55,35 @@ namespace Atmosphere
             if (audioSource != null)
                 audioSource.Stop();
 
+            if (ivaAudioSource != null)
+                ivaAudioSource.Stop();
+
             if (ambientSoundGameObject != null)
                 GameObject.Destroy(ambientSoundGameObject);
 
             GameEvents.onGameSceneLoadRequested.Remove(GameSceneLoadStarted);
+            GameEvents.OnCameraChange.Remove(CameraChanged);
         }
 
         public void Update(float coverage)
         {
             if (coverage < 0.05)
+            { 
                 audioSource.Pause();
+
+                if (ivaAudioSource != null)
+                    ivaAudioSource.Pause();
+            }
             else
             {
-                audioSource.volume = coverage;
+                audioSource.volume = coverage * (ivaPlaying ? 0.8f : 1f);
                 audioSource.UnPause();
+
+                if (ivaAudioSource != null && ivaPlaying)
+                {
+                    ivaAudioSource.volume = coverage;
+                    ivaAudioSource.UnPause();
+                }
             }
 
         }
@@ -71,13 +95,32 @@ namespace Atmosphere
                 ambientSoundGameObject.SetActive(value);
 
                 if (!value)
+                {
                     audioSource.Pause();
+
+                    if (ivaAudioSource != null)
+                        ivaAudioSource.Pause();
+                }
+                    
             }
         }
 
         private void GameSceneLoadStarted(GameScenes scene)
         {
             Update(0f);
+        }
+
+        private void CameraChanged(CameraManager.CameraMode cameraMode)
+        {
+            if (ivaAudioSource != null && (cameraMode == CameraManager.CameraMode.IVA || cameraMode == CameraManager.CameraMode.Internal))
+            {
+                ivaPlaying = true;
+            }
+            else
+            { 
+                ivaPlaying = false;
+                ivaAudioSource.Pause();
+            }
         }
     }
 }
