69416e711e95cb1bab4b17e6760843580c6e6c85
lghassen <feeldapowa@hotmail.com>
2024-03-29 11:55:54 +0100

    Revert "Also reproject light volume in less passes", doesn't work on OpenGL and we don't need it with directx (we have compute)
    This reverts commit cca5b48b28955cb8cf0481f7316503ffdb78ac02.

--- Atmosphere/RaymarchedClouds/LightVolume/LightVolume.cs
+++ Atmosphere/RaymarchedClouds/LightVolume/LightVolume.cs
@@ -373,22 +373,24 @@ namespace Atmosphere
 
         private void ReprojectSlices(RenderTexture targetRT)
         {
-            for (int i = 0; i < volumeSlices; i += maxSlicesInOnePass)
+            reprojectLightVolumeMaterial.SetFloat(ShaderProperties.ambientLightVolume_PROPERTY, 0f);
+
+            for (int i = 0; i < volumeSlices; i++)
             {
-                int slicesToUpdateThisPass = Math.Min(volumeSlices - i, maxSlicesInOnePass);
+                float verticalUV = ((float)i + 0.5f) / (float)(volumeSlices);
+                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.verticalUV_PROPERTY, verticalUV);
 
-                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.startSlice_PROPERTY, i);
-                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.slicesToUpdate_PROPERTY, slicesToUpdateThisPass);
+                RenderTextureUtils.Blit3D(targetRT, i, mergedVolumeSlices, reprojectLightVolumeMaterial, 0);
+            }
 
-                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.ambientLightVolume_PROPERTY, 0f);
+            reprojectLightVolumeMaterial.SetFloat(ShaderProperties.ambientLightVolume_PROPERTY, 1f);
 
-                Graphics.SetRenderTarget(targetRT, 0, CubemapFace.Unknown, -1);
-                Graphics.Blit(null, reprojectLightVolumeMaterial, 0, -1);
+            for (int i = 0; i < volumeSlices; i++)
+            {
+                float verticalUV = ((float)i + 0.5f) / (float)(volumeSlices);
+                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.verticalUV_PROPERTY, verticalUV);
 
-                reprojectLightVolumeMaterial.SetFloat(ShaderProperties.ambientLightVolume_PROPERTY, 1f);
-
-                Graphics.SetRenderTarget(targetRT, 0, CubemapFace.Unknown, -1);
-                Graphics.Blit(null, reprojectLightVolumeMaterial, 0, -1);
+                RenderTextureUtils.Blit3D(targetRT, volumeSlices + i, mergedVolumeSlices, reprojectLightVolumeMaterial, 0);
             }
         }
     }
