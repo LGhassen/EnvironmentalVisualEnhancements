0f9ef0901a4192c0d9aeaed4fd60ea0ba0c9d350
lghassen <11978271+LGhassen@users.noreply.github.com>
2026-04-15 21:36:50 +0200

    Increase GUI window size

--- EveManager/GlobalEVEManager.cs
+++ EveManager/GlobalEVEManager.cs
@@ -142,7 +142,7 @@ namespace EVEManager
 
         #pragma warning disable 0649
         private GUISkin _mySkin;
-        private Rect _mainWindowRect = new Rect(0, 0, 600, 720);
+        private Rect _mainWindowRect = new Rect(0, 0, 700, 900);
 
         protected static int selectedManagerIndex = 0;
         
@@ -151,8 +151,8 @@ namespace EVEManager
             GUI.skin = _mySkin;
             if (useEditor)
             {
-                _mainWindowRect.width = 600;
-                _mainWindowRect.height = 720;
+                _mainWindowRect.width = 700;
+                _mainWindowRect.height = 900;
                 var versionInfo = Assembly.GetExecutingAssembly().GetName().Version.ToString();
                 _mainWindowRect = GUI.Window(0x8100, _mainWindowRect, DrawMainWindow, "EVE "+versionInfo+" Config Editor");
             }
